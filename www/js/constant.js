509bcc41bd9807ed37ef8dea1cbdf5daafad9cc9
ash11109 <ayushcool1275@gmail.com>
2025-04-01 03:37:17 +0530

    config url set

--- www/js/constant.js
+++ www/js/constant.js
@@ -14,3 +14,5 @@ const Config = {
     domain : "https://crm-2025-ten.vercel.app/www/",
     autoAssignUrl : "https://teamka.in/crm1/APIs/ash_auto_lead_assign.php"
 };
+
+// "https://teamka.in/crm1/APIs/api_development.php"
